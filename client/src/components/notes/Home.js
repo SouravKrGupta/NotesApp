f390d306bfb87db78275fb7544c39b78567849bb
Sourav Kumar Gupta <89971045+SouravKrGupta@users.noreply.github.com>
2023-06-22 11:49:25 +0530

    sharing work done

--- client/src/components/notes/Home.js
+++ client/src/components/notes/Home.js
@@ -5,10 +5,17 @@ import axios from "axios";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import ShareIcon from "@mui/icons-material/Share";
+import Popover from "@mui/material/Popover";
+import IconButton from "@mui/material/IconButton";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import GmailIcon from "@mui/icons-material/Mail";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 export default function Home() {
   const [notes, setNotes] = useState([]);
   const [token, setToken] = useState("");
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [currentNote, setCurrentNote] = useState(null);
 
   const getNotes = async (token) => {
     const res = await axios.get("api/notes", {
@@ -37,27 +44,45 @@ export default function Home() {
       window.location.href = "/";
     }
   };
-  //Share part
-  const shareNote = async (id, title, content, date) => {
-    try {
-      if (token) {
-       
-        // Share on Twitter
-        const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(title + " - " + content + " " + format(date) )}`;
-        window.open(twitterUrl, "_blank");
-      }
-    } catch (error) {
-      console.error("Error sharing note:", error);
-    }
+
+  const shareNote = (event, note) => {
+    setCurrentNote(note); // Set the currently selected note
+    setAnchorEl(event.currentTarget);
   };
+
+  const closeSharePopover = () => {
+    setAnchorEl(null);
+  };
+
+  const shareViaWhatsApp = (title, content, date) => {
+    const encodedTitle = encodeURIComponent(title);
+    const encodedContent = encodeURIComponent(content);
+    const encodedDate = encodeURIComponent(format(date));
+    const whatsappUrl = `https://api.whatsapp.com/send?text=${encodedTitle}%20-%20${encodedContent}%20${encodedDate}`;
+    window.open(whatsappUrl, "_blank");
+  };
+
+  const shareViaGmail = (title, content, date) => {
+    const encodedTitle = encodeURIComponent(title);
+    const encodedContent = encodeURIComponent(content);
+    const encodedDate = encodeURIComponent(format(date));
+    const gmailUrl = `mailto:?subject=${encodedTitle}&body=${encodedContent}%20${encodedDate}`;
+    window.location.href = gmailUrl;
+  };
+
+  const shareViaTwitter = (title, content, date) => {
+    const encodedTitle = encodeURIComponent(title);
+    const encodedContent = encodeURIComponent(content);
+    const encodedDate = encodeURIComponent(format(date));
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${encodedTitle}%20-%20${encodedContent}%20${encodedDate}`;
+    window.open(twitterUrl, "_blank");
+  };
+
   useEffect(() => {
     const handleCardColors = () => {
       const cards = document.querySelectorAll(".card");
       cards.forEach((card) => {
-        card.style.setProperty(
-          "--card-bg-color",
-          getRandomColor()
-        );
+        card.style.setProperty("--card-bg-color", getRandomColor());
       });
     };
 
@@ -90,11 +115,63 @@ export default function Home() {
           <button className="delete" onClick={() => deleteNote(note._id)}>
             <DeleteIcon />
           </button>
-             <button className="share" onClick={() => shareNote(note._id, note.title, note.content, note.date)}>
-            
-            <ShareIcon/>
-            
-          </button>
+          <IconButton
+            className="share"
+            onClick={(event) => shareNote(event, note)}
+          >
+            <ShareIcon />
+          </IconButton>
+          <Popover
+            open={Boolean(anchorEl)}
+            anchorEl={anchorEl}
+            onClose={closeSharePopover}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+          >
+            {currentNote && (
+              <>
+                <IconButton
+                  onClick={() =>
+                    shareViaWhatsApp(
+                      currentNote.title,
+                      currentNote.content,
+                      currentNote.date
+                    )
+                  }
+                >
+                  <WhatsAppIcon />
+                </IconButton>
+                <IconButton
+                  onClick={() =>
+                    shareViaGmail(
+                      currentNote.title,
+                      currentNote.content,
+                      currentNote.date
+                    )
+                  }
+                >
+                  <GmailIcon />
+                </IconButton>
+                <IconButton
+                  onClick={() =>
+                    shareViaTwitter(
+                      currentNote.title,
+                      currentNote.content,
+                      currentNote.date
+                    )
+                  }
+                >
+                  <TwitterIcon />
+                </IconButton>
+              </>
+            )}
+          </Popover>
         </div>
       ))}
     </div>
