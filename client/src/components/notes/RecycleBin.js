a22f30881a954fa7921c9d8fe0e30cf96775f0fa
Sourav Kumar Gupta <89971045+SouravKrGupta@users.noreply.github.com>
2023-09-05 16:59:14 +0530

    css update

--- client/src/components/notes/RecycleBin.js
+++ client/src/components/notes/RecycleBin.js
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+
 import axios from "axios";
 import DeleteIcon from "@mui/icons-material/Delete";
 import RestoreIcon from "@mui/icons-material/Restore";
